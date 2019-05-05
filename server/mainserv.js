1ba5c9960668b2ebe026156746c4c22b41736f25
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-05-06 01:12:38 +0200

    Added a main loop cycle function to the server.

--- server/mainserv.js
+++ server/mainserv.js
@@ -1,5 +1,6 @@
 
 var access_counter = 0;
+var cycle = 0;
 
 var increment_access_counter = function(){
   ++access_counter;
@@ -14,10 +15,19 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   increment_access_counter();
-  res.end("This is klaim\'s multiplayer js game prototype, yay! Access = " + access_counter + "\/n");
+  res.end("This is klaim\'s multiplayer js game prototype, yay! Access = " + access_counter 
+    + ", cycle = " + cycle + "\/n");
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at ${hostname}:${port}`);
 });
+
+var update = function(){
+  ++cycle;
+
+};
+
+setInterval(update, 100); // Run the game update
+
 
