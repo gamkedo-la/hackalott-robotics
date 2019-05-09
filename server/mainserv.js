1013365491aa005054d9103d998ca8a93c2f997c
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-05-09 21:15:47 +0200

    Deactivates websocket code for now.

--- server/mainserv.js
+++ server/mainserv.js
@@ -144,44 +144,44 @@ const update_and_restart = () => {
   update_sources_to_master(restart);
 };
 
-var WebSocketServer = require('websocket').server;
+// var WebSocketServer = require('websocket').server;
 
-var wsServer = new WebSocketServer({
-  httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
-  autoAcceptConnections: false
-});
+// var wsServer = new WebSocketServer({
+//   httpServer: server,
+//   // You should not use autoAcceptConnections for production
+//   // applications, as it defeats all standard cross-origin protection
+//   // facilities built into the protocol and the browser.  You should
+//   // *always* verify the connection's origin and decide whether or not
+//   // to accept it.
+//   autoAcceptConnections: false
+// });
 
-function clientIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
+// function clientIsAllowed(origin) {
+//   // put logic here to detect whether the specified origin is allowed.
+//   return true;
+// }
 
-wsServer.on('request', function(request) {
-  if (!clientIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
-    request.reject();
-    console.log((new Date()) + ' Connection from client ' + request.origin + ' rejected.');
-    return;
-  }
+// wsServer.on('request', function(request) {
+//   if (!clientIsAllowed(request.origin)) {
+//     // Make sure we only accept requests from an allowed origin
+//     request.reject();
+//     console.log((new Date()) + ' Connection from client ' + request.origin + ' rejected.');
+//     return;
+//   }
   
-  var connection = request.accept('echo-protocol', request.origin);
-  console.log((new Date()) + ' Connection accepted.');
-  connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          console.log('Received Message: ' + message.utf8Data);
-          connection.sendUTF(message.utf8Data);
-      }
-      else if (message.type === 'binary') {
-          console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-          connection.sendBytes(message.binaryData);
-      }
-  });
-  connection.on('close', function(reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
-});
+//   var connection = request.accept('echo-protocol', request.origin);
+//   console.log((new Date()) + ' Connection accepted.');
+//   connection.on('message', function(message) {
+//       if (message.type === 'utf8') {
+//           console.log('Received Message: ' + message.utf8Data);
+//           connection.sendUTF(message.utf8Data);
+//       }
+//       else if (message.type === 'binary') {
+//           console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+//           connection.sendBytes(message.binaryData);
+//       }
+//   });
+//   connection.on('close', function(reasonCode, description) {
+//       console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+//   });
+// });
