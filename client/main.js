965da8b90080b1fca72d40ffe0bef9a6d7c5e349
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-05-20 01:23:23 +0200

    Interractive connection interface.

--- client/main.js
+++ client/main.js
@@ -3,6 +3,15 @@ var default_server_hostname = "https://prototypenodejs.klaimsden.net"; // Used i
 
 let field = {
     server_hostname : document.getElementById('server_hostname'),
+    connection_panel : document.getElementById('connection_panel'),
+    server_hostname : document.getElementById('server_hostname'),
+    button_server_reset : document.getElementById('button_server_reset'),
+    button_play_online : document.getElementById('button_play_online'),
+    button_play_offline : document.getElementById('button_play_offline'),
+    connection_status_panel: document.getElementById('connection_status_panel'),
+    game_panel : document.getElementById('game_panel'),
+    online_info_box : document.getElementById('online_info_box'),
+    online_info_box_opener : document.getElementById('online_info_box_opener'),
 
     reset_server_hostname : ()=>{ server_hostname.value = default_server_hostname; },
     set_default_server : (new_default_url)=>{ default_server_hostname = new_default_url; }
@@ -29,9 +38,10 @@ const connect_to_server = (server_url)=>{
             + default_server_hostname + " or " + from_http_to_websocket(default_server_hostname);
     }
 
-    console.log(`Connecting to ${ws_url} through WebSocket...`);
-    websocket = new WebSocket(ws_url);
+    hide_connection_panel();
+    show_connection_status(`Connecting to ${ws_url} through WebSocket...`);
 
+    websocket = new WebSocket(ws_url);
     websocket.addEventListener("error", on_received_error);
     websocket.addEventListener("message", on_received_message);
     websocket.addEventListener("open", on_connection_open);
@@ -39,21 +49,68 @@ const connect_to_server = (server_url)=>{
 };
 
 const on_received_error = (error) => {
-    console.error("Connection to server ERROR: " + error);
+    let log = "Server Error: " + error;
+    console.error(log);
+    show_connection_status(log);
+    stop_game_client();
 };
 
 const on_received_message = (message) => {
-    console.log("Received message: " + message);
+    console.log("Received message: " + message.data);
+    // TODO: pass the message to the game handler
 };
 
 const on_connection_open = (event) => {
-    console.log("Connection to server: OK");
+    show_connection_status("Connection to server: OK");
+    start_game_client();
+    setTimeout(hide_connection_status, 2000); // Hide the connection status a bit later.
 };
 
 const on_connection_closed = (event) => {
-    console.log("Connection to server: CLOSED");
+    show_connection_status("Connection to server: CLOSED");
+    stop_game_client();
 }
 
+const log_connection_status = (message) => {
+    console.log(message);
+    field.connection_status_panel.textContent = message;
+    // TODO: add to the log instead of overwritting it.
+}
 
+const show_connection_status = (message = "") => {
+    if(message.length != 0) {
+        log_connection_status(message);
+    }
+    field.connection_status_panel.style.display = "block";
+}
+const hide_connection_status = () => {
+    field.connection_status_panel.style.display = "none";
+}
 
+const show_connection_panel = () => {
+    connection_panel.style.display = "block";
+}
 
+const hide_connection_panel = () => {
+    connection_panel.style.display = "none";
+}
+
+const start_game_client = () => {
+    game_panel.style.display = "block";
+    // TODO: inject game client display here and start update here.
+}
+
+const stop_game_client = () => {
+    game_panel.style.display = "none";
+    game_panel.innerText = "";
+}
+
+const open_info_box = () => {
+    field.online_info_box.style.display = "block";
+    field.online_info_box_opener.style.display = "none";
+}
+
+const close_info_box = () => {
+    field.online_info_box.style.display = "none";
+    field.online_info_box_opener.style.display = "block";
+}
