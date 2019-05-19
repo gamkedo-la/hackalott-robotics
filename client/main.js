67802bb1975452442345ddfe6422bb9b045ed209
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-05-19 19:05:59 +0200

    Server specifies the public hostname and set it by default inside the client page.

--- client/main.js
+++ client/main.js
@@ -1,15 +1,15 @@
 
-var default_server_url = "prototypenodejs.klaimsden.net";
+var default_server_hostname = "prototypenodejs.klaimsden.net"; // Used if the client is run in a local browser.
 
 let field = {
-    server_url : document.getElementById('server_url'),    
+    server_hostname : document.getElementById('server_hostname'),
 
-    reset_server_url : ()=>{ server_url.value = default_server_url; },
-    set_default_url : (new_default_url)=>{ default_server_url = new_default_url; }
+    reset_server_hostname : ()=>{ server_hostname.value = default_server_hostname; },
+    set_default_server : (new_default_url)=>{ default_server_hostname = new_default_url; }
 
 };
 
-field.reset_server_url();
+field.reset_server_hostname();
 
 let client = {
     websocket: undefined, // Until conenct_to_server() is called.
--- server/mainserv.js
+++ server/mainserv.js
@@ -31,7 +31,25 @@ const processPortFromArgs = () => {
   }
 
   return port;
-}
+};
+
+const default_hostname = `127.0.0.1`;
+
+const processPublicHostnameFromArgs = () => {
+  var hostname = process.argv[3]; // We assume that the argument after port is the hostname
+  if(!hostname) {
+    let generated_hostname = `${default_hostname}:${port}`;
+    console.warn("Unspecified public hostname as 3rd argument: defaulting to " + generated_hostname);
+    hostname = generated_hostname;
+  }
+  
+  if(hostname.length <= 3) {
+    throw "Public host name is not valid: " + hostname;
+  }
+
+  console.log("PUBLIC HOSTNAME: " + hostname);
+  return hostname;
+};
 
 const gameloop = require('./gameloop');
 gameloop.start(); // Start the game even if there is no connections yet.
@@ -41,47 +59,53 @@ const ws_server = require("./websocketserv");
 const admin = require('./admin');
 const html = require('./html_utils');
 
-const hostname = '127.0.0.1';       // TODO: make this an optional CLI parametter
+const path = require('path');
+const hostname = default_hostname;       // TODO: make this an optional CLI parametter
 const port = processPortFromArgs();
+const public_hostname = processPublicHostnameFromArgs();
 
 const http_server = http.createServer((req, res) => {
   if(req.method=="GET")
   {
+    if(req.url=="/")
+    {
+      increment_access_counter();
+      html.serve("index.html", res, {
+        "access_counter" : access_counter,
+        "cycle" : gameloop.update_cycle(),
+        "client_count" : ws_server.count_clients(),
+        "server_code" : "document.getElementById('online_info').style.display='block';"
+            + `\nfield.set_default_server('${public_hostname}');`
+            + "\nfield.reset_server_hostname();"
 
+      });
+    }
+    else if(req.url=="/admin")
+    {
+      html.serve("server/admin.html", res);
+    }
+    else if(req.url=="/restart") {
+      html.serve("server/restarting.html", res).then(admin.update_and_restart);
+    }
+    else if(req.url=="/quick-restart") {
+      html.serve("server/restarting.html", res).then(admin.restart);
+    }
+    else if(req.url=="/stop") {
+      html.serve("server/stopped.html", res).then(admin.stop);
+    }
+    // We allow accessing all client files.
+    else if(req.url.startsWith("/client/") || req.url.startsWith("/core/"))
+    {
+      html.serve(req.url, res);
+    }  
+    // Request not understood or file not found:
+    else {
+      html.serve_file_not_found(req.url, res);
+    }  
   }
-  if(req.url=="/")
-  {
-    increment_access_counter();
-    html.serve("index.html", res, {
-      "access_counter" : access_counter,
-      "cycle" : gameloop.update_cycle(),
-      "client_count" : ws_server.count_clients(),
-      "server_code" : "document.getElementById('online_info').style.display='block';\n"
-          + `field.set_default_url(window.location.hostname);`
-    });
-  }
-  else if(req.url=="/admin")
-  {
-    html.serve("server/admin.html", res);
-  }
-  else if(req.url=="/restart") {
-    html.serve("server/restarting.html", res).then(admin.update_and_restart);
-  }
-  else if(req.url=="/quick-restart") {
-    html.serve("server/restarting.html", res).then(admin.restart);
-  }
-  else if(req.url=="/stop") {
-    html.serve("server/stopped.html", res).then(admin.stop);
-  }
-  // We allow accessing all client files.
-  else if(req.url.startsWith("/client/") || req.url.startsWith("/core/"))
-  {
-    html.serve(req.url, res);
-  }  
-  // Request not understood or file not found:
   else {
     html.serve_file_not_found(req.url, res);
-  }  
+  }
 });
 
 http_server.listen(port, hostname, () => {
